ab8db3492d5e017a165ac004a5867f9ab8a44cbb
Bryan Davis <bd808@bd808.com>
2008-11-17 05:42:00 +0000

    More project boilerplate.
    git-svn-id: https://python-iptools.googlecode.com/svn/trunk@5 99efc558-b41a-11dd-8714-116ca565c52f

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from ez_setup import use_setuptools
+use_setuptools()
 
-setup(name = 'iptools',
-      description = 'Python utilited for manipulating IP addresses',
-      version = '0.1',
-      url = 'http://python-iptools.googlecode.com',
-      author = 'Bryan Davis',
-      author_email = 'casadebender+iptools@gmail.com',
-      license = 'BSD',
-      packages = ['iptools',],
-      classifiers = ['Development Status :: 3 - Alpha', 'Topic :: Internet', ]
-    )
+from setuptools import setup, find_packages
+
+setup(
+    name = 'iptools',
+    description = 'Python utilited for manipulating IP addresses',
+    version = '0.1',
+    url = 'http://python-iptools.googlecode.com',
+    author = 'Bryan Davis',
+    author_email = 'casadebender+iptools@gmail.com',
+    license = 'BSD',
+    platforms = ['any',],
+    packages = find_packages('src'),
+    classifiers = [
+      'Development Status :: 3 - Alpha', 
+      'Environment :: Web Environment',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Utilities',
+      'Topic :: Internet', 
+    ],
+    zip_safe=False,
+)
