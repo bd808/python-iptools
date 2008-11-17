4751364bf1a5821bc350376252cfcd0223a9fa75
Bryan Davis <bd808@bd808.com>
2008-11-17 05:52:41 +0000

    More setup.py config.
    git-svn-id: https://python-iptools.googlecode.com/svn/trunk@6 99efc558-b41a-11dd-8714-116ca565c52f

--- setup.py
+++ setup.py
@@ -5,16 +5,22 @@ use_setuptools()
 
 from setuptools import setup, find_packages
 
+version = '0.1'
+
 setup(
     name = 'iptools',
-    description = 'Python utilited for manipulating IP addresses',
-    version = '0.1',
+    version = version,
+    description = 'Python utilites for manipulating IP addresses',
+    long_description = "Utilities for manipulating IP addresses including a class that can be used to include CIDR network blocks in Django's INTERNAL_IPS setting.",
     url = 'http://python-iptools.googlecode.com',
+    download_url = '',
     author = 'Bryan Davis',
     author_email = 'casadebender+iptools@gmail.com',
     license = 'BSD',
     platforms = ['any',],
+    package_dir = {'': 'src'},
     packages = find_packages('src'),
+    include_package_data = True,
     classifiers = [
       'Development Status :: 3 - Alpha', 
       'Environment :: Web Environment',
