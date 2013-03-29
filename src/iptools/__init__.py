62691b5a8554d82c5bc788bc8213253c694213ba
Bryan Davis <bd808@bd808.com>
2013-03-28 21:38:51 -0600

    Bump copyright date.

--- src/iptools/__init__.py
+++ src/iptools/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2008-2012, Bryan Davis
+# Copyright (c) 2008-2013, Bryan Davis
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
